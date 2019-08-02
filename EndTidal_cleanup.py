a247b4ded87f8ce079255d9e9f4ab591fe9f6985
jacek257 <jacek257@gmail.com>
2019-08-02 12:26:55 -0700

    graphs are now on one png file and output is resampled to 320

--- EndTidal_cleanup.py
+++ EndTidal_cleanup.py
@@ -6,10 +6,10 @@ By: Jimi Cao, Austin Sophonsri
 import os
 import pandas as pd
 from scipy import signal as sg
-from scipy import interpolate
 import argparse
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 # instantiate the argument parser
 parser = argparse.ArgumentParser()
@@ -41,29 +41,30 @@ sns.set(rc={'figure.figsize':(20,10)})
 
 for file in txt_files:
     if verb:
-        print("Processing ", file)
+        print("Checking ", file)
 
     # combine file name with path to get the full path of the file
     f_path = path+file
-    # read in the data from the text files, but only keep the Time, O2, and CO2 data
-    df = pd.read_csv(f_path, sep='\t|,', names=['Time', 'O2', 'CO2', 'thrw', 'away'], usecols=['Time', 'O2', 'CO2'], index_col=False, engine='python')
-
-    df = df.dropna()
-
-    if df.empty:
-        print(file, " dropped. File did not contain the correct amount of data")
-        continue
 
     # make the directory that the contrast data will be saved to if directory doesn't exist
     if not os.path.exists(f_path[:-4]):
         os.mkdir(f_path[:-4])
     else:
         continue
+    
+    # read in the data from the text files, but only keep the Time, O2, and CO2 data
+    df = pd.read_csv(f_path, sep='\t|,', names=['Time', 'O2', 'CO2', 'thrw', 'away'], usecols=['Time', 'O2', 'CO2'], index_col=False, engine='python')
 
+    df = df.dropna()
+
+    if df.empty or df.CO2.max() < 1:
+        print(file, "dropped. Bad data")
+        continue
+    
     # make a loop for user confirmation that O2 peak detection is good
     bad = True
-    prom = 2
-    wid = None
+    prom = 1
+    wid = 1
     while bad:
         # get the troughs of the O2 data
         low_O2, _ = sg.find_peaks(df.O2.apply(lambda x:x*-1), prominence=prom, width=wid)
@@ -75,42 +76,33 @@ for file in txt_files:
         sns.lineplot(x='Time', y='O2', data=df, linewidth=1, color='b')
 
         # add peak overlay onto the scatterplot
-        sns.lineplot(x='Time', y='O2', data=df.iloc[low_O2], linewidth=4, color='g')
+        sns.lineplot(x='Time', y='O2', data=df.iloc[low_O2], linewidth=2, color='g')
         plt.show()
         plt.close()
         
         # ask user if the peak finding was good
         ans = input("Was the output good enough (y/n)? \nNote: anything not starting with 'y' is considered 'n'.\n")
-        bad = False if ans[0].lower() == 'y' else True
+        bad = True if ans == '' or ans[0].lower() != 'y' else False
         if bad:
-            print("There are 2 variables that can be changed: ")
+            print("The following variables can be changed: ")
             print("    1. prominence - Required prominence of peaks. Type: int")
-            print("    2. width      - Required width of peaks in samples. Type: int")
+            print("    2. width      - Required distance between peaks. Type: int")
             try:
-                prom = int(input("New prominence (Default is 2): "))
+                prom = int(input("New prominence (Default is 1): "))
             except:
                 print("Default value used")
-                prom = 2
+                prom = 1
             try:
-                wid = int(input("New width (Default is None): "))
+                prom = int(input("New width (Default is 1): "))
             except:
                 print("Default value used")
-                wid=None
-        else:
-            # save the plot
-            if verb:
-                print("Saving O2 plot ", file)    
-            # recreate the plot because plt.show clears plt
-            sns.lineplot(x='Time', y='O2', data=df, linewidth=1, color='b')
-            sns.lineplot(x='Time', y='O2', data=df.iloc[low_O2], linewidth=2, color='g')
-            save_path = path+file[:len(file)-4]+'/O2_graph.png'
-            plt.savefig(save_path)
-            plt.close()
+                wid = 1
+    
 
     # make another loop for user confirmation that CO2 peak detection is good
     bad = True
-    prom = 2
-    wid = None
+    prom = 1
+    wid = 1
     while bad:
         # get peaks of the CO2 data
         high_CO2, _ = sg.find_peaks(df.CO2, prominence=prom, width=wid)
@@ -122,52 +114,69 @@ for file in txt_files:
         sns.lineplot(x='Time', y='CO2', data=df, linewidth=1, color='b')
     
         # add peak overlay onto the scatterplot
-        sns.lineplot(x='Time', y='CO2', data=df.iloc[high_CO2], linewidth=4, color='r')
+        sns.lineplot(x='Time', y='CO2', data=df.iloc[high_CO2], linewidth=2, color='r')
         plt.show()
         plt.close()
         
         # ask user if the peak finding was good
         ans = input("Was the output good enough (y/n)? \nNote: anything not starting with 'y' is considered 'n'.\n")
-        bad = False if ans[0].lower() == 'y' else True
+        bad = True if ans == '' or ans[0].lower() != 'y' else False
         if bad:
-            print("There are 2 variables that can be changed: ")
+            print("The following variables can be changed: ")
             print("    1. prominence - Required prominence of peaks. Type: int")
-            print("    2. width      - Required width of peaks in samples. Type: int")
+            print("    2. width      - Required distance between peaks. Type: int")
             try:
-                prom = int(input("New prominence (Default is 2): "))
+                prom = int(input("New prominence (Default is 1): "))
             except:
                 print("Default value used")
-                prom = 2
+                prom = 1
             try:
-                wid = int(input("New width (Default is None): "))
+                prom = int(input("New width (Default is 1): "))
             except:
                 print("Default value used")
-                wid=None
-        else:
-            # save the plot
-            if verb:
-                print('Saving CO2 plot ', file)
-            # recreate the plot because plt.show clears plt
-            sns.lineplot(x='Time', y='CO2', data=df, linewidth=1, color='b')
-            sns.lineplot(x='Time', y='CO2', data=df.iloc[high_CO2], linewidth=2, color='g')
-            save_path = save_path = path+file[:len(file)-4]+'/CO2_graph.png'
-            plt.savefig(save_path)
-            plt.close()
+                wid = 1
+        
+    plt.close()
+
+    # create subplots for png file later
+    f, axes = plt.subplots(2, 1)            
+    
+    # recreate the plot because plt.show clears plt
+    sns.lineplot(x='Time', y='O2', data=df, linewidth=1, color='b', ax=axes[0])
+    sns.lineplot(x='Time', y='O2', data=df.iloc[low_O2], linewidth=2, color='g', ax=axes[0])
+
+    # save the plot
+    if verb:
+        print('Saving plots for', file)
+    # recreate the plot because plt.show clears plt
+    sns.lineplot(x='Time', y='CO2', data=df, linewidth=1, color='b', ax=axes[1])
+    sns.lineplot(x='Time', y='CO2', data=df.iloc[high_CO2], linewidth=2, color='r', ax=axes[1])
+    
+    save_path = save_path = path+file[:len(file)-4]+'/graph.png'
+#    plt.savefig(save_path)
+    f.savefig(save_path)
+    if verb:
+        print('Saving complete')
+    f.clf()
+#    plt.close()
 
     # since the find_peaks only returns the index that the peak is found, we need to grab the actual data point
     O2_df = df.iloc[low_O2].O2
     CO2_df = df.iloc[high_CO2].CO2
 
+    O2_resamp = sg.resample(O2_df, 320)
+    CO2_resamp = sg.resample(CO2_df, 320)
+
     # make the file name and save the O2 and CO2 data into their corresponding file
     if verb:
         print("Saving O2 data for ", file)
     save_path = path+file[:len(file)-4]+'/O2_contrast.txt'
-    O2_df.to_csv(path_or_buf=save_path, sep='\t', header=False, index=False)
+    np.savetxt(save_path, O2_resamp, delimiter='\t')
 
     if verb:
         print('Saving CO2 data for ', file)
     save_path = path+file[:len(file)-4]+'/CO2_contrast.txt'
-    CO2_df.to_csv(path_or_buf=save_path, sep='\t', header=False, index=False)
+    np.savetxt(save_path, CO2_resamp, delimiter='\t')
 
     if verb:
         print()
