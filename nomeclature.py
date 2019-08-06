d51910d2ce8358a927f14b35001b48fdcb6bf68c
Tamagognai <au.sophonsri@gmail.com>
2019-08-06 14:06:12 -0700

    initialized entidal cleaning algorithm

--- nomeclature.py
+++ nomeclature.py
@@ -3,7 +3,8 @@ By: Austin Sophonsri
 
 '''
 
-import os
+import os,sys
+import subprocess
 import argparse
 import pandas as pd
 
@@ -44,7 +45,8 @@ p_df = pd.DataFrame({
                 'ID':[f[3:6] for f in txt_files],
                 'Month':[f[11:13] for f in txt_files],
                 'Day':[f[13:15] for f in txt_files],
-                'Year':[f[7:11] for f in txt_files]
+                'Year':[f[7:11] for f in txt_files],
+                'EndTidal_Path': [path+f for f in txt_files]
              })
 
 print(p_df.head())
@@ -68,9 +70,9 @@ for i in range(len(p_df)):
     b_files = [file for file in os.listdir(nifti_dir + p_df.Cohort[i] + p_df.ID[i] +'/BOLD/') if file.endswith('a.nii')]
     fs_files = [file for file in os.listdir(freesurfer_t1_dir) if file == p_df.Cohort[i]+p_df.ID[i]+'_FS_T1.nii.gz']
 
-    #select file
-    nii_paths['BOLD_path'].append(b_files[0] if len(b_files) > 0 else '')
-    nii_paths['T1_path'].append(fs_files[0] if len(fs_files) > 0 else '')
+    #select and add file to appropriate list
+    nii_paths['BOLD_path'].append(nifti_dir + p_df.Cohort[i] + p_df.ID[i] +'/BOLD/'+b_files[0] if len(b_files) > 0 else '')
+    nii_paths['T1_path'].append(freesurfer_t1_dir+fs_files[0] if len(fs_files) > 0 else '')
     nii_paths['boldFS_exists'].append(len(b_files) > 0 and len(fs_files)>0)
 
 
@@ -80,12 +82,49 @@ p_df = pd.concat((p_df, pd.DataFrame(nii_paths)), axis=1)
 #drop all false conditional rows and conditional column
 p_df = p_df[p_df.boldFS_exists != False].drop('boldFS_exists', axis = 1)
 
-print(p_df.sort_values('ID'))
+print('\n',p_df.head())
 
-#run EndTidal Cleaning and return paths
+#get number of volumes
+#run fslinfo and pipe -> python buffer -> replace \n with ' ' -> split into list -> choose correct index -> convert string to int
+p_df["Dimension"] = [int(subprocess.run(['fslinfo' ,b_path], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\n',' ').split()[9]) for b_path in p_df.BOLD_path]
+#choose non-trivial bold series
+p_df = p_df[p_df.Dimension >=150]
+print('\n',p_df.head())
 
+####run EndTidal Cleaning and return paths
+#make endtidal folders
 
+ET_dict = {'ETO2' : [], 'ETCO2' : [], 'ET_exists' : []}
 
+for f_path in p_df.EndTidal_Path:
+    if not os.path.exists(f_path[:-4]):
+        os.mkdir(f_path[:-4])
+
+    #perfrom cleaning and load into p_df
+    #load data into DataFrame
+    df = pd.read_csv(f_path, sep='\t|,', names=['Time', 'O2', 'CO2', 'thrw', 'away'], usecols=['Time', 'O2', 'CO2'], index_col=False, engine='python')
+    #drop rows with missing cols
+    df = df.dropna()
+
+    #skip if DataFrame is empty
+    if df.empty:
+        os.rmdir(f_path[:-4])
+        ET_dict['ETO2'].append('')
+        ET_dict['ETCO2'].append('')
+        ET_dict['ET_exists'].append(False)
+        continue
+
+    # need to scale CO2 data is necessary
+    if df.CO2.max() < 1:
+        df.CO2 = df.CO2 * 100
+
+    #get fourier cleaned data
+    #generate and store cleaned data paths
+    print(df.O2[:5])
+    print(df.CO2[:5])
+    #save data
+
+#run Feat
 
 
 
