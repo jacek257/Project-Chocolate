a4cbd2c232269379362bce597b78b95eb01b56b7
Austin Sophonsri <au.sophonsri@gmail.com>
2019-08-14 16:09:45 -0700

    tested optimizer class  and refactored all modules into analysis.py

--- centroid.py
+++ centroid.py
@@ -15,10 +15,14 @@ def get_centroid(t_series, data_series, window_coeff, poly_order):
     """
     centroid is a signal weighted average of time. We are essentially calculating the temporal middle of the signal
 
-    t_series (iterable) = time points of data_series
-    data_series (iterable) = the signal intensity series
-    window_coeff (int) = filter window size
-    poly_order (int) = the order of savgol fit polynomial
+    params:
+        t_series (iterable) = time points of data_series
+        data_series (iterable) = the signal intensity series
+        window_coeff (int) = filter window size
+        poly_order (int) = the order of savgol fit polynomial
+
+    return:
+        (float)
     """
     #set buffer to 0
     buffer = 0.0
@@ -32,15 +36,17 @@ def align_centroid(base_time, base_sig, time1, sig1, time2, sig2): #TODO: arbitr
     """
     aligns several (3) time series based on their relative temporal centroids
 
-    base_time = BOLD
-    rest = self explanatory
+    params:
+        base_time = BOLD
+        rest = self explanatory
+        *time* (iterable) = time points / sampling points
+        *sig* (iterable) = sampling intensity at each time/sampling points
 
-    *time* (iterable) = time points / sampling points
-    *sig* (iterable) = sampling intensity at each time/sampling points
+    warnings:
+        [*]sig[*] must have equal length to [*]time[*]
 
-    [*]sig[*] must have equal length to [*]time[*]
-
-    returns a dictionary containing  {base_time, base_sig, sig1, sig2} properly aligned and interpolated
+    returns:
+        (dictionary) {base_time, base_sig, sig1, sig2}
     """
     #get all centroids
     base_centroid = get_centroid(base_time, base_sig)
@@ -61,27 +67,32 @@ def grad_constant(c1,c2,c3,s1n, s2n, bn):
     """
     part of mathematical gradient which is present in all components
 
-    c1 (float) = constant 1
-    c2 (float) = constant 2
-    c3 (float) = constant 3
+    params:
+        c1 (float) = constant 1
+        c2 (float) = constant 2
+        c3 (float) = constant 3
+        s1n (float) = signal1 at time n
+        s2n (float) = signal2 at time n
+        bn (float) = base signal at time n
 
-    s1n (float) = signal1 at time n
-    s2n (float) = signal2 at time n
-    bn (float) = base signal at time n
+    returns:
+        (float)
     """
     return(2*(c1*s1n + c2*s2n + c3 - bn))
 def grad_C1(C1, C2, C3, S1, S2, B):
     """
     component 1 of gradient
 
-    C1 (float) = constant 1
-    C2 (float) = constant 2
-    C3 (float) = constant 3
+    params:
+        C1 (float) = constant 1
+        C2 (float) = constant 2
+        C3 (float) = constant 3
+        S1 (iterable) = signal 1
+        S2 (iterable) = signal 2
+        B (iterable) = base signal
 
-    S1 (iterable) = signal 1
-    S2 (iterable) = signal 2
-
-    B (iterable) = base signal
+    returns:
+        (float)
     """
     buffer = 0.0
     for s1n, s2n, bn in zip(S1, S2, B):
@@ -92,14 +103,16 @@ def grad_C2(C1, C2, C3, S1, S2, B):
     """
     component 2 of gradient
 
-    C1 (float) = constant 1
-    C2 (float) = constant 2
-    C3 (float) = constant 3
+    params:
+        C1 (float) = constant 1
+        C2 (float) = constant 2
+        C3 (float) = constant 3
+        S1 (iterable) = signal 1
+        S2 (iterable) = signal 2
+        B (iterable) = base signal
 
-    S1 (iterable) = signal 1
-    S2 (iterable) = signal 2
-
-    B (iterable) = base signal
+    returns:
+        (float)
     """
     buffer = 0.0
     for s1n, s2n, bn in zip(S1, S2, B):
@@ -110,14 +123,16 @@ def grad_C3(C1, C2, C3, S1, S2, B):
     """
     component 3 of gradient
 
-    C1 (float) = constant 1
-    C2 (float) = constant 2
-    C3 (float) = constant 3
+    params:
+        C1 (float) = constant 1
+        C2 (float) = constant 2
+        C3 (float) = constant 3
+        S1 (iterable) = signal 1
+        S2 (iterable) = signal 2
+        B (iterable) = base signal
 
-    S1 (iterable) = signal 1
-    S2 (iterable) = signal 2
-
-    B (iterable) = base signal
+    returns:
+        (float)
     """
     buffer = 0.0
     for s1n, s2n, bn in zip(S1, S2, B):
@@ -127,15 +142,20 @@ def grad_C3(C1, C2, C3, S1, S2, B):
 def linear_optimize(S1, S2, B, init_tuple = (0,0,0), descent_speed=.1, lifespan = 100):
     """
     linear coefficient optimizer by gradient descent
-    returns (C1, C2, C3)
-    where: C1 * S1 + C2 * S2 + C3 = B
 
-    init_tuple (c1, c2, c3) = initial coefficeint guess
-    S1 (iterable) = signal 1
-    S2 (iterable) = signal 2
-    B (iterable) = base signal
-    descent_speed (float) = descending step size
-    lifespan = number of descending steps
+    params:
+        S1 (iterable) = signal 1
+        S2 (iterable) = signal 2
+        B (iterable) = base signal
+        init_tuple (c1, c2, c3) = initial coefficeint guess
+        descent_speed (float) = descending step size
+        lifespan = number of descending steps
+
+    Warnings:
+        non-stochastic gradient descent is extremely sensitive to local minima
+
+    returns:
+        (C1 (float), C2 (float), C3 (float)) where: C1 * S1 + C2 * S2 + C3 = B
     """
         factor1 = np.max(S1)
     factor2 = np.max(S2)
@@ -162,7 +182,22 @@ def linear_optimize(S1, S2, B, init_tuple = (0,0,0), descent_speed=.1, lifespan 
     B = B*factorB
     return return_tuple
 
-def stochastic_optimize(S1, S2, B, init_tuple = (0,0,0), descent_speed=.1, lifespan = 100):
+def stochastic_optimize(S1, S2, B, init_tuple = (0,0,0), descent_speed=.1, lifespan = 100, p_factor = .9): #TODO: look-ahead grad. descent
+        """
+        linear coefficient optimizer by stochastic gradient descent
+
+        params:
+            S1 (iterable) = signal 1
+            S2 (iterable) = signal 2
+            B (iterable) = base signal
+            init_tuple (c1, c2, c3) = initial coefficeint guess
+            descent_speed (float) = descending step size
+            lifespan = number of descending steps
+            p_factor = momentum constant (determines the contribution of momentum and slope)
+
+        returns:
+            (C1 (float), C2 (float), C3 (float)) where: C1 * S1 + C2 * S2 + C3 = B
+        """
     factor1 = np.max(S1)
     factor2 = np.max(S2)
     factorB = np.max(B)
@@ -190,7 +225,7 @@ def stochastic_optimize(S1, S2, B, init_tuple = (0,0,0), descent_speed=.1, lifes
 
 
     return_tuple = (curr_C1*factorB/factor1, curr_C2*factorB/factor2, curr_C3*factorB)
-    
+
     S1 = S1*factor1
     S2 = S2*factor2
     B = B*factorB
